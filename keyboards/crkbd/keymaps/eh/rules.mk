eddbb8bb9d49de030021f8765bfa4c173dd4b821
kissetfall <bogpill@ya.ru>
2021-11-17 22:09:56 +0300

    fifth

--- keyboards/crkbd/keymaps/eh/rules.mk
+++ keyboards/crkbd/keymaps/eh/rules.mk
@@ -1,5 +1,6 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
 MOUSEKEY_ENABLE     = no     # Mouse keys
 RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
 VIA_ENABLE          = yes         # Enable VIA
-OLED_DRIVER_ENABLE  = yes
 LTO_ENABLE          = yes
