ddbd77986ec1f0950f6dd89110798176f5d98478
otmaxxx <otmaxxx@users.noreply.github.com>
2022-02-14 15:57:50 +0300

    Update keymap.c
    right encoder - mouse wheel up down

--- keyboards/sofle/keymaps/ehrgb/keymap.c
+++ keyboards/sofle/keymaps/ehrgb/keymap.c
@@ -344,9 +344,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         }
     } else if (index == 1) {
         if (clockwise) {
-            tap_code(KC_PGDN);
+            tap_code(KC_WH_D);
         } else {
-            tap_code(KC_PGUP);
+            tap_code(KC_WH_U);
         }
     }
     return true;
